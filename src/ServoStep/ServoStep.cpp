f861a3cc9fa25be3bb801ddae2fa3de08cc59c33
Louis <lourou99@gmail.com>
2025-07-02 22:57:50 +0200

    Removed servoUp & servoDown

--- src/ServoStep/ServoStep.cpp
+++ src/ServoStep/ServoStep.cpp
@@ -2,46 +2,41 @@
 #include "ServoStep.h"
 #include "../globals.h"
 
-ServoStep::ServoStep(int pin)
+// Updating the servo with PWN signals, as there is no servo lib for esp32-c3
+// * Works great with the servo plugged into 3v and not 5v.
+
+ServoStep::ServoStep(int pin, int channel)
 {
     _pin = pin;
-    ledcSetup(0, 200, 8);
-    ledcAttachPin(_pin, 0);
+    _channel = channel;
+    ledcSetup(_channel, 200, 8);
+    ledcAttachPin(_pin, _channel);
 }
 
 void ServoStep::servoSet(int angle)
 {
     duty = (map(angle, 0, 180, 5, 25)) * 5.1;
-    // 5.1 is the ratio of pulse width (in decimilliseconds)to duty cycle at a fequency of 200Hz (the minimum for ledc on esp32c3)
-    ledcWrite(0, duty); // write to servo
+    ledcWrite(_channel, duty);
     delay(300);
-}
-
-void ServoStep::servoUp()
-{
-    ledcWrite(0, 77);  // 90degress
-    ledcWrite(0, 128); // 180 degrees
-    delay(300);
-    ledcWrite(0, 77); // 90 degrees
-}
-
-void ServoStep::servoDown()
-{
-    ledcWrite(0, 77); // 90degress
-    ledcWrite(0, 26); // 0 degrees
-    delay(300);
-    ledcWrite(0, 77); // 90 degrees
 }
 
 // Forward declare fmap
 float fmap(float x, float in_min, float in_max, float out_min, float out_max);
 
+// Drive wheels forward or backwards.
 void ServoStep::servoSpin(int speed)
 {
-    // float duty = fmap(speed, -512.0, 512.0, 122.0, 130.0);
     float duty = fmap(speed, -512.0, 512.0, 120.0, 130.0);
-    ledcWrite(0, duty);
+    ledcWrite(_channel, duty);
     Dev::log(String(speed) + " | " + String(duty));
+}
+
+// Turn wheels left or right.
+void ServoStep::servoTurn(int angle)
+{
+    float duty = fmap(angle, -512.0, 512.0, 50.0, 95.0);
+    ledcWrite(_channel, duty);
+    Dev::log(String(angle) + " -|- " + String(duty));
 }
 
 float fmap(float x, float in_min, float in_max, float out_min, float out_max)
